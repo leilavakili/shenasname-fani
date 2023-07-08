77a42c74e667d33803778c852d8cd97807cbd901
Leila Vakili <leilavakili2002@gmail.com>
2023-07-09 02:11:56 +0330

    9th

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,15 +1,16 @@
 import React, { Component } from 'react'
 import "./Footer.css";
-import Certificate from './BuildingCertificate-Small.svg';
+import Pic from './BuildingCertificate-Small.svg';
+
 
 class Footer extends Component{
 
 render(){
     return(
         <footer>
-        <img src={Certificate} className="Certificate" alt="Certificate"/>
-        <a>تمامی حقوق این سایت برای سازمان نظام مهندسی استان قم محفوظ است</a>
-        <a>اجرا و پیاده سازی توسط:اندیشه فناوری اطلاعات قرن</a>    
+        <img src={Pic} className="Pic" alt="Pic"/>
+        <a className='aright'>تمامی حقوق این سایت برای سازمان نظام مهندسی استان قم محفوظ است</a>
+        <a className='aleft'>اجرا و پیاده سازی توسط:اندیشه فناوری اطلاعات قرن</a>    
         </footer>
     )
 }
--- src/components/Table/Table.js
+++ src/components/Table/Table.js
@@ -7,7 +7,6 @@ class Table extends Component{
 
 render(){
     return(
-
 <table>
 
     <tr class="white-row row-1" id="head">
