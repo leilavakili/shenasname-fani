884b828117ddac3615b0ea70bd5914db0dc9f650
Leila Vakili <leilavakili2002@gmail.com>
2023-07-08 18:36:57 +0330

    8th

--- src/App.js
+++ src/App.js
@@ -1,17 +1,11 @@
 import './App.css';
-import Footer from './components/Footer/Footer';
-import Navbar from './components/Navbar/Navbar';
-import Navbar2 from './components/Navbar2/Navbar2';
-import Table from './components/Table/Table';
-
+import { Route, Routes } from "react-router-dom"
+import First from './pages/First';
 function App() {
   return (
-    <div className="App">
-    <Navbar />
-    <Navbar2 />
-    <Table />
-    <Footer />
-    </div>
+   <Routes>
+    <Route path="/" element={<First />} />
+   </Routes>
   );
 }
 
---
+++ src/pages/First.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+import Navbar from '../components/Navbar/Navbar';
+import Navbar2 from '../components/Navbar2/Navbar2';
+import Table from '../components/Table/Table';
+import Footer from '../components/Footer/Footer';
+
+
+class First extends Component{
+
+render(){
+    return(
+        <><Navbar /><Navbar2 /><Table /><Footer /></>
+        )
+    }
+}
+    export default First;
